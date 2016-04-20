3db6cfec5972f14d0c93a5934cbebd3ecc4801ac
Arnaud Bailly <arnaud.oqube@gmail.com>
2016-04-20 16:12:34 +0200

    add host-only networking to ubuntu 14.04 vagrant box

--- scripts/mirage.sh
+++ scripts/mirage.sh
@@ -3,4 +3,10 @@
 set -ex
 
 sudo apt-get install -y libssl-dev pkg-config # required by many mirage apps
-opam install mirage --yes                     # install mirage
+
+# update opam installation
+opam switch $(opam switch | grep Official | tail -1 | awk '{print $3}')
+eval $(opam config env)
+
+# install mirage 
+opam install mirage --yes
--- scripts/networking.sh
+++ scripts/networking.sh
@@ -4,7 +4,35 @@ set -ex
 
 apt-get install -y dnsmasq avahi-daemon # networking
 
-cat <<EOF
+cat  >> /etc/dnsmasq.conf <<EOF
 interface=br0
-dhcp-range=192.168.56.150,192.168.56.200,1h
-EOF >> /etc/dnsmasq.conf
+dhcp-range=192.168.77.3,192.168.77.200,1h
+EOF
+
+# activate IP forwarding
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
+
+# set eth1 in manual mode
+cat  > /tmp/update-eth1 <<EOF
+s/iface eth1 inet static/iface eth1 inet manual/
+/address 192.168.77.2/d
+/netmask 255.255.255.0/d
+EOF
+
+sed -i"" -f /tmp/update-eth1 /etc/network/interfaces
+
+# setup bridge br0 interface 
+cat  >> /etc/network/interfaces <<EOF
+auto br0
+iface br0 inet static
+    bridge_ports eth1
+    address 192.168.77.2
+    broadcast 192.168.77.255
+    netmask 255.255.255.0
+EOF
+
+# stop eth1 then activate both interfaces
+ifdown eth1
+ifup br0
+
